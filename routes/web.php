3f4433d3546c7cbc349f1ab04ccea62639ce52a8
Vmjlol <maniezzojr@yahoo.com.br>
2024-04-02 22:50:49 -0300

    Commit One to One

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+//use App\Models\Preference;
+//use App\Models\User;
+
+use App\Models\{
+    User,
+    Preference
+};
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +20,31 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/one-to-one', function () {
+    $user = User::with('preference')->find(2); //with()
+
+
+    $data = [
+        'background_color' => '#080',
+    ];
+
+    if ($user->preference) {
+        $user->preference->update($data);
+    } else {
+        //$user->preference()->create($data);
+        $preference = new Preference($data);
+        $user->preference()->save($preference);
+    }
+    $user->refresh();
+
+    var_dump($user->preference);
+
+    $user->preference->delete();
+    $user->refresh();
+
+    dd($user->preference);
+});
 
 Route::get('/', function () {
     return view('welcome');
